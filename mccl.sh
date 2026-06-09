454e37a0329653342653035b4c61704249ac24a0
papertager <2567587994@qq.com>
2026-06-09 21:36:34 +0800

    Make MCCL smoke parameters configurable

--- mccl.sh
+++ mccl.sh
@@ -30,8 +30,15 @@ fi
 
 MPI_PROCESS_NUM=${GPU_NUM}
 MPI_RUN_OPT="--allow-run-as-root -mca pml ^ucx -mca osc ^ucx -mca btl ^openib"
+MCCL_MIN_BYTES="${MCCL_MIN_BYTES:-1K}"
+MCCL_MAX_BYTES="${MCCL_MAX_BYTES:-1G}"
+MCCL_DTYPE="${MCCL_DTYPE:-bfloat16}"
+MCCL_STEP_FACTOR="${MCCL_STEP_FACTOR:-2}"
+MCCL_GPUS_PER_PROCESS="${MCCL_GPUS_PER_PROCESS:-1}"
+MCCL_ITERS="${MCCL_ITERS:-10}"
 
 for BENCH in ${BENCH_NAMES}; do
 echo -n "The test is ${BENCH}, the maca version is " && realpath ${MACA_PATH}
-${MACA_PATH}/ompi/bin/mpirun -np ${MPI_PROCESS_NUM} ${MPI_RUN_OPT} ${TEST_DIR}/${BENCH} -b 1K -e 1G -d bfloat16 -f 2 -g 1 -n 10
+echo "MCCL params: min=${MCCL_MIN_BYTES} max=${MCCL_MAX_BYTES} dtype=${MCCL_DTYPE} step=${MCCL_STEP_FACTOR} gpus_per_process=${MCCL_GPUS_PER_PROCESS} iters=${MCCL_ITERS}"
+${MACA_PATH}/ompi/bin/mpirun -np ${MPI_PROCESS_NUM} ${MPI_RUN_OPT} ${TEST_DIR}/${BENCH} -b "${MCCL_MIN_BYTES}" -e "${MCCL_MAX_BYTES}" -d "${MCCL_DTYPE}" -f "${MCCL_STEP_FACTOR}" -g "${MCCL_GPUS_PER_PROCESS}" -n "${MCCL_ITERS}"
 done
